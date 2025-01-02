e977f5676a6bdf5b10978369549ad209ebfabc9d
skksharif <khasimsharif12@gmail.com>
2025-01-02 12:55:26 +0530

    first commit

--- app.py
+++ app.py
@@ -83,5 +83,6 @@ def ask():
     enhanced_answer = enhance_answer_with_gemini(user_question, base_answer)
     return jsonify({"answer": enhanced_answer})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Use Render's port or default to 5000
+    app.run(host="0.0.0.0", port=port)
